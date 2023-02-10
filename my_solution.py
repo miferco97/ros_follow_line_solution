1042e6adb5c28feba17c7e9ff76c527953767349
Miguel <miferco97@gmail.com>
2023-02-10 14:52:40 +0100

    bounce reduced

--- my_solution.py
+++ my_solution.py
@@ -10,6 +10,9 @@ from cv_bridge import CvBridge
 from rclpy.qos import QoSProfile
 from rclpy.qos import QoSReliabilityPolicy, QoSHistoryPolicy
 
+# SPEED = 20.0
+SPEED = 10.0
+
 qos_profile = QoSProfile(
     reliability=QoSReliabilityPolicy.RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT,
     history=QoSHistoryPolicy.RMW_QOS_POLICY_HISTORY_KEEP_LAST,
@@ -22,6 +25,7 @@ im_width = 0
 im_height = 0
 
 last_error = 0
+last_mode = 0
 
 def filter_red_line(frame):
     frameHSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)        
@@ -31,9 +35,9 @@ def filter_red_line(frame):
 
 
 def control_yaw(frame,current_frame):
-    global last_error
+    global last_error,last_mode,SPEED
     # print(np.shape(frame))
-    row = int (0.55 * im_height)
+    row = int (0.53 * im_height)
     # print(im_height,im_width)
     see_line = frame[row,:]
     print(row, len(see_line))
@@ -48,24 +52,34 @@ def control_yaw(frame,current_frame):
     image = cv2.circle(current_frame, (medium,row), 5, (250,0,0),-1)
     print(f'{error=}')
     msg = Twist()
-    x_speed = 20.0
+    x_speed = SPEED
     msg.linear.x = x_speed
     if abs(error) < 20:
+        mode = 0
         msg.angular.z = float (error /70)
     elif abs(error) < 40:
+        mode = 1
         msg.angular.z = float (error /60)
     elif abs(error) < 50:
+        mode = 2
         msg.angular.z = float (error /50)
     elif abs(error) < 70:
+        mode = 3
         msg.angular.z = float (error /30)
     else:
+        mode = 4
         msg.angular.z = float (error /15)
+
+    if mode <2 and last_mode >=3:
+    #     print('converging')
+         msg.angular.z *= 0.8
 
     # if abs(last_error - error) > 30 and error < 30 :
     #     print('converging')
     #     msg.angular.z *= 0.8
 
     last_error = error
+    last_mode = mode
 
     return msg
     
