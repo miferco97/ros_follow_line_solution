ce1f8a4dbf07e5094e74843cc7b50c3a13efc817
Miguel <miferco97@gmail.com>
2023-02-10 14:23:44 +0100

    20 m/s

---
+++ my_solution.py
@@ -0,0 +1,122 @@
+import rclpy
+import cv2
+import numpy as np
+
+from rclpy.node import Node
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from geometry_msgs.msg import Twist
+from cv_bridge import CvBridge
+from rclpy.qos import QoSProfile
+from rclpy.qos import QoSReliabilityPolicy, QoSHistoryPolicy
+
+qos_profile = QoSProfile(
+    reliability=QoSReliabilityPolicy.RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT,
+    history=QoSHistoryPolicy.RMW_QOS_POLICY_HISTORY_KEEP_LAST,
+    depth=1)
+
+redBajo1 = np.array([0, 100, 20], np.uint8)
+redAlto1 = np.array([30, 255, 255], np.uint8)
+
+im_width = 0
+im_height = 0
+
+last_error = 0
+
+def filter_red_line(frame):
+    frameHSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)        
+    maskRed = cv2.inRange(frameHSV, redBajo1, redAlto1)
+    cv2.imshow('frame', maskRed)
+    return maskRed
+
+
+def control_yaw(frame,current_frame):
+    global last_error
+    # print(np.shape(frame))
+    row = int (0.55 * im_height)
+    # print(im_height,im_width)
+    see_line = frame[row,:]
+    print(row, len(see_line))
+    see_line = list(see_line)
+    begin = see_line.index(255)
+    see_line.reverse()
+    end=len(see_line) - see_line.index(255)
+    # print(begin,end)
+    medium = int((end+begin)/2)
+    medium_ref = im_width/2
+    error = medium_ref - medium
+    image = cv2.circle(current_frame, (medium,row), 5, (250,0,0),-1)
+    print(f'{error=}')
+    msg = Twist()
+    x_speed = 20.0
+    msg.linear.x = x_speed
+    if abs(error) < 20:
+        msg.angular.z = float (error /70)
+    elif abs(error) < 40:
+        msg.angular.z = float (error /60)
+    elif abs(error) < 50:
+        msg.angular.z = float (error /50)
+    elif abs(error) < 70:
+        msg.angular.z = float (error /30)
+    else:
+        msg.angular.z = float (error /15)
+
+    # if abs(last_error - error) > 30 and error < 30 :
+    #     print('converging')
+    #     msg.angular.z *= 0.8
+
+    last_error = error
+
+    return msg
+    
+
+
+
+class Pilot(Node):
+
+    def __init__(self):
+        super().__init__('miguel_pilot')
+        self.subscription = self.create_subscription(
+            Image,
+            'cam_f1/image_raw',
+            self.listener_callback,
+            qos_profile)
+        self.subscription  # prevent unused variable warning
+        self.publisher_ = self.create_publisher(Twist, 'cmd_vel', 10)
+        self.br = CvBridge()
+
+    def listener_callback(self, msg: Image):
+        current_frame = self.br.imgmsg_to_cv2(msg)
+        global im_height, im_width
+        im_width = msg.width
+        im_height = msg.height
+        current_frame = cv2.cvtColor(current_frame,cv2.COLOR_BGR2RGB)
+        line_filtered = filter_red_line(current_frame)
+        try:
+            out = control_yaw(line_filtered,current_frame)
+            self.publisher_.publish(out)
+            cv2.imshow('image',current_frame)
+        except:
+            cv2.imshow('image',current_frame)
+            cv2.waitKey(20)
+            raise Exception('Error')
+            pass
+        cv2.waitKey(20)
+        # self.get_logger().info('I heard: "%s"' % msg.data)
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    node = Pilot()
+
+    rclpy.spin(node)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
